d53df2c35c25efe420775dc31bcef5e2daf6dc5b
L1xxxYEY <sergeevamarinaaaa2004@gmail.com>
2025-02-15 21:11:45 +0500

    rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Welcome from "./pages/Welcome";
-import StepOne from "./pages/StepOne";
 import "./styles/main.css";
 
 const App = () => {
   return (
     <div className="App">
-      <StepOne />
+      <Welcome />
     </div>
   );
 };
